4d62bd037e50ad55c8fc227cb76dc7ecfbdcb39f
Rafael Eller <rafael@cscruz.org>
2019-02-23 09:46:10 -0300

    Add data_input.py as well as its tests

---
+++ src/data_input.py
@@ -0,0 +1,230 @@
+from collections import namedtuple
+import datetime
+
+import numpy as np
+from openpyxl import load_workbook
+
+
+class ConsistencyBetweenFilesError(Exception):
+    pass
+
+
+Person = namedtuple(
+    'Person', ['name', 'gender', 'exp_level', 'task_answ'])
+
+ProblemInfo = namedtuple('ProblemInfo', [
+                         'n_p_units', 'n_days', 'n_tasks', 'tasks', 'names', 'days',
+                         'people_per_task', 'women_per_task',
+                         'parents_per_task', 'n_people', 'n_women', 'n_parents',
+                         'reject', 'force'])
+
+# TODO Add some checks
+
+
+def read_ficha_servo(file_path, id_column='A', exp_column='B', gend_column='S',
+                     task_columns=['G', 'H', 'I', 'J',
+                                   'K', 'L', 'M', 'N', 'O'],
+                     name_column='R'):
+    """ Reads the Ficha Servo spreadsheet.
+    Args:
+        file_path: (str) The full path to the 'ficha_servo' file.
+        id_column: (str) The column containing the id for the people.
+        exp_column: (str) The column containing the experience level.
+        gend_column: (str) The column containing the gender of the person.
+        name_column: (str) The column containing the name of each person.
+        task_columns: (list) A list containing all the letters of the columns
+    containing tasks.
+    Returns:
+        tasks: (list) The list with all the available tasks.
+        people: (dict) The dict of form {id:PersonNamedTuple}
+    """
+    wb = load_workbook(file_path)
+    sheet = wb.active
+
+    column_to_task = {}
+    for column in task_columns:
+        column_to_task[column] = sheet[column + '1'].value
+    tasks = list(column_to_task.values())
+
+    idx = 2
+    people = {}
+    # NOTE Here I use a max number of lines equal to 1000
+    while sheet[id_column + str(idx)].value is not None and idx < 1000:
+        task_answ = {
+            column_to_task[column]: sheet[column + str(idx)].value for column in task_columns}
+        person = Person(name=sheet[name_column + str(idx)].value,
+                        gender=sheet[gend_column + str(idx)].value,
+                        exp_level=sheet[exp_column + str(idx)].value,
+                        task_answ=task_answ)
+        people[sheet[id_column + str(idx)].value] = person
+        idx += 1
+    return tasks, people
+
+
+def read_demanda(file_path, people_column='B', women_column='C', parents_column='D'):
+    """ Reads the demanda file that specifies the number of people demanded
+    by each task, as well as the number of women and experienced people.
+    Args:
+    Returns:
+
+    """
+    wb = load_workbook(file_path)
+    sheet = wb.active
+    idx = 2
+    tasks = []
+    people_demand = {}
+    women_demand = {}
+    parents_demand = {}
+    while sheet['A' + str(idx)].value is not None and idx < 1000:
+        tasks.append(sheet['A' + str(idx)].value)
+        people_demand[sheet['A' +
+                            str(idx)].value] = sheet[people_column + str(idx)].value
+        women_demand[sheet['A' +
+                           str(idx)].value] = sheet[women_column + str(idx)].value
+        parents_demand[sheet['A' +
+                             str(idx)].value] = sheet[parents_column + str(idx)].value
+        idx += 1
+    return tasks, people_demand, women_demand, parents_demand
+
+
+def read_solution_sheet(file_path, id_column='A', name_column='C',
+                        day_columns=['D', 'E', 'F', 'G']):
+    """ Reads the solution sheet with the indisponibilities and pre-allocations,
+    as well as previous days infos to define completely the instance.
+    Args:
+        file_path: (str) The full path to the solution sheet.
+        id_column: (str) The column containing the id of the people
+        name_column: (str) The column containing the names of each person.
+        day_columns: (list) A list containing the columns of the days to be solved.
+    Returns:
+        days: (list) A list with the names of the days
+        sheet_info: (list) A list with the triples (id, day name, info) where
+            info can be one of the tasks or an indisponibility.
+    """
+    wb = load_workbook(file_path)
+    sheet = wb.active
+    col_to_day = {}
+    for column in day_columns:
+        value = sheet[column + '1'].value
+        if isinstance(value, datetime.datetime):
+            col_to_day[column] = value.strftime('%d/%m/%Y')
+        elif value is str:
+            col_to_day = value
+        else:
+            raise Exception('Unknown type for date')
+
+    sheet_info = []
+    idx = 2
+    while sheet[id_column + str(idx)].value is not None and idx < 1000:
+        id_ = sheet[id_column + str(idx)].value
+        for column in day_columns:
+            day = col_to_day[column]
+            info = sheet[column + str(idx)].value
+            if info is not None:
+                sheet_info.append((id_, day, info))
+        idx += 1
+    days = list(col_to_day.values())
+    return days, sheet_info
+
+
+def get_problem_info(ficha_servo_path, demanda_path, sol_sheet_path,
+                     exp_threshold=3):
+    """ Gets the complete info from the sheets to define the problem
+    Args:
+        ficha_servo_path: (str) 
+        demanda_path: (str)
+        sol_sheet_path: (str)
+        exp_threshold: (str)
+    Returns:
+        problem_info: (namedtuple)
+    """
+    tasks, people = read_ficha_servo(ficha_servo_path)
+    tasks_check, people_demand, women_demand, parents_demand = read_demanda(
+        demanda_path)
+    days, sheet_info = read_solution_sheet(sol_sheet_path)
+
+    if tasks != tasks_check:
+        raise ConsistencyBetweenFilesError("The names of the tasks are different "
+                                           "between the 'ficha servo' and 'demanda' files")
+    n_p_units = len(people)
+    n_days = len(days)
+    n_tasks = len(tasks)
+
+    # The list is extracted from the dict following the 'tasks' list, in order
+    # to guarantee they are both in the same order.
+    people_demand_list = [people_demand[task] for task in tasks]
+    women_demand_list = [women_demand[task] for task in tasks]
+    parents_demand_list = [parents_demand[task] for task in tasks]
+
+    # NOTE The repetition assumes that the demand is a single list, i.e., that
+    # the demands are specified only once, and equal on all days.
+    people_per_task = np.repeat(np.array(people_demand_list)[
+                                np.newaxis, :], n_days, axis=0)
+    women_per_task = np.repeat(np.array(women_demand_list)[
+                               np.newaxis, :], n_days, axis=0)
+    parents_per_task = np.repeat(np.array(parents_demand_list)[
+                                 np.newaxis, :], n_days, axis=0)
+
+    id_to_i = {}
+    names = []
+    n_people = []
+    n_women = []
+    n_parents = []
+    reject = []
+    force = []
+    for i, (id_, person) in enumerate(people.items()):
+        id_to_i[id_] = i
+        names.append(person.name)
+        # NOTE since I'm not yet allowing a multi-person p_unit, every p_unit
+        # has 1 and only 1 person on it.
+        n_people.append(1)
+        n_women.append(1) if person.gender == 'F' else n_women.append(0)
+        n_parents.append(1) if person.exp_level >= exp_threshold \
+            else n_parents.append(0)
+        for task, answ in person.task_answ.items():
+            k = tasks.index(task)
+            if answ == "Não Aceito":
+                # Add a rejection for the given task on each day
+                # NOTE: So far I'm not using the distiction between 'Gosto' and
+                # 'Aceito'.
+                reject += [(i, j, k) for j in range(len(days))]
+
+    for id_, day, info in sheet_info:
+        i = id_to_i[id_]
+        j = days.index(day)
+        # If the cell contains a task, then the person is forced to perform it
+        # on the given day
+        if info in tasks:
+            k = tasks.index(info)
+            force.append((i, j, k))
+        # If the cell contains 'indisp' the person is unavailable during that
+        # day and thus rejected on all tasks for that day.
+        elif info == 'indisp':
+            reject += [(i, j, k) for k in range(len(tasks))]
+
+    # Remove repeated triples.
+    force = list(set(force))
+    reject = list(set(reject))
+
+    problem_info = ProblemInfo(n_p_units=n_p_units,
+                               n_days=n_days,
+                               n_tasks=n_tasks,
+                               tasks=tasks,
+                               names=names,
+                               days=days,
+                               people_per_task=people_per_task,
+                               women_per_task=women_per_task,
+                               parents_per_task=parents_per_task,
+                               n_people=n_people,
+                               n_women=n_women,
+                               n_parents=n_parents,
+                               reject=reject,
+                               force=force)
+
+    return problem_info
+
+
+if __name__ == '__main__':
+    PATH = 'C:\\Users\\Rafael\\dev\\sapi_scheduler\\data\\Example_Experiencia.xlsx'
+    tasks, people = read_ficha_servo(PATH)
+    print(tasks)
---
+++ tests/test_data_input.py
@@ -0,0 +1,144 @@
+#! /usr/bin/env python
+import unittest
+from contextlib import contextmanager
+from datetime import datetime
+import os.path as osp
+
+import numpy as np
+
+from src.data_input import read_ficha_servo, read_demanda, read_solution_sheet, get_problem_info
+
+
+class DataInputTest(unittest.TestCase):
+    """
+    """
+
+    def test_ficha_servo(self):
+        """
+        Tests the reading of the ficha servo type file
+        """
+        dirname = osp.dirname(__file__)
+        ficha_servo_path = osp.join(dirname, 'Example_Ficha_Servo.xlsx')
+
+        tasks, people = read_ficha_servo(ficha_servo_path)
+
+        # The test sheet should have 65 people on it
+        self.assertEqual(len(people), 65)
+        # The test sheet should have 9 tasks.
+        self.assertEqual(len(tasks), 9)
+
+        # Check that the only task answers are 'Não Aceito', 'Aceito'
+        # and 'Gosto'.
+        for id_, person in people.items():
+            for answ in person.task_answ.values():
+                self.assertTrue(answ in ['Não Aceito', 'Aceito', 'Gosto'])
+            for task in person.task_answ.keys():
+                self.assertTrue(task in tasks)
+        # Check that all genders are 'F' or 'M'
+        for id_, person in people.items():
+            self.assertTrue(person.gender in ['F', 'M'])
+
+    def test_demanda(self):
+        """
+        """
+        dirname = osp.dirname(__file__)
+        ficha_servo_path = osp.join(dirname, 'Example_Ficha_Servo.xlsx')
+        demanda_path = osp.join(dirname, 'Example_Demanda.xlsx')
+
+        tasks_ficha, people = read_ficha_servo(ficha_servo_path)
+
+        tasks_demanda, people_demand, women_demand, parents_demand = read_demanda(
+            demanda_path)
+
+        # Check that the tasks are the same for both files.
+        self.assertTrue(len(tasks_demanda) == len(tasks_ficha))
+        for task in tasks_demanda:
+            self.assertTrue(task in tasks_ficha)
+
+    def test_solution_sheet(self):
+        """
+        """
+        dirname = osp.dirname(__file__)
+        sol_sheet_path = osp.join(dirname, 'Example_Indisponibilidade.xlsx')
+        # Pay close attention to the date formatting
+        days_expected = ['03/02/2019', '10/02/2019',
+                         '17/02/2019', '24/02/2019']
+        tasks = ['BercCM', 'BercEBD', 'BercCV', 'MatCM',
+                 'MatEBD', 'MatCV', 'JardEBD', 'JuvEBD', 'indisp',
+                 'not_allocated']
+
+        days, sheet_info = read_solution_sheet(sol_sheet_path)
+        # Assert that the function reads the dates correctly
+        self.assertTrue(days == days_expected)
+        for triple in sheet_info:
+            # Assert that all the days info is contained in the days list
+            self.assertTrue(triple[1] in days)
+            # Assert that all info belongs to one of the tasks, is 'indisp' or
+            # 'not_allocated'.
+            self.assertTrue(triple[2] in tasks)
+
+    def test_get_problem_info(self):
+        """
+        """
+        dirname = osp.dirname(__file__)
+        ficha_servo_path = osp.join(dirname, 'Example_Ficha_Servo.xlsx')
+        sol_sheet_path = osp.join(dirname, 'Example_Indisponibilidade.xlsx')
+        demanda_path = osp.join(dirname, 'Example_Demanda.xlsx')
+
+        problem_info = get_problem_info(
+            ficha_servo_path, demanda_path, sol_sheet_path)
+
+        self.assertTrue(problem_info.n_p_units == 65)
+        self.assertTrue(problem_info.n_days == 4)
+        self.assertTrue(len(problem_info.tasks) == 9)
+
+        expected_tasks = ['BercCM', 'BercEBD', 'BercCV', 'MatCM',
+                          'MatEBD', 'MatCV', 'JardEBD', 'JuvEBD', 'MusEBD']
+        self.assertTrue(sorted(problem_info.tasks) == sorted(expected_tasks))
+        self.assertTrue(len(problem_info.names) == 65)
+        # Check two random names
+        self.assertTrue('Carol Cruz' in problem_info.names and
+                        'Vanessa Andrade' in problem_info.names)
+        n_women = [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0,
+                   1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+                   0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1,
+                   1, 1, 1, 1, 1, 0, 1, 0]
+        n_parents = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                     1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                     1, 1, 1, 1, 1, 0, 0, 0]
+        self.assertTrue(problem_info.n_women == n_women)
+        self.assertTrue(problem_info.n_parents == n_parents)
+
+        # Check some random preferences (Péricles Pereira: 22,
+        # and Gabriela Seabra Gomes Mustafá: 25), given that they have no
+        # unavailabilities the only force and reject triples come from the
+        # 'ficha servo'.
+        reject_Pericles = []
+        reject_Gabi = []
+        for reject in problem_info.reject:
+            if reject[0] == 22:
+                reject_Pericles.append(reject)
+            if reject[0] == 26:
+                reject_Gabi.append(reject)
+        expected_reject_Pericles = [(22, j, k) for j in range(
+            problem_info.n_days) for k in [0, 2, 3, 5, 8]]
+        expected_reject_Gabi = [(26, j, k) for j in range(
+            problem_info.n_days) for k in [1, 4, 6, 7, 8]]
+
+        self.assertTrue(sorted(expected_reject_Pericles)
+                        == sorted(reject_Pericles))
+        self.assertTrue(sorted(expected_reject_Gabi) == sorted(reject_Gabi))
+
+        # Also there should be no forced allocations for either
+        for force in problem_info.force:
+            self.assertTrue(force[0] != 22 and force[0] != 26)
+
+        # There should be forced allocations for Carol Martins:29,
+        # Ana Rafaela:20, Bruna Oliveira:37 and Rafael:53.
+        expected_force = [(29, 0, 0), (20, 0, 0), (37, 0, 7), (53, 0, 7)]
+        self.assertTrue(sorted(expected_force) == sorted(problem_info.force))
+
+
+if __name__ == '__main__':
+    unittest.main()
